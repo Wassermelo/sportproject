cd57b7d16f0b4c8e0f774051e09024aff512b84a
Wassermelo <48138585+Wassermelo@users.noreply.github.com>
2022-09-27 14:39:15 +0200

    Update Main.js

--- src/register/Main.js
+++ src/register/Main.js
@@ -2,6 +2,7 @@
 By Ahmed Al-Ghezi
  */
 
+
 import React, {useState} from 'react';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
@@ -22,35 +23,32 @@ import SignOut from "./sign-out";
 import EditCoach from "./trainer/editCoach";
 import CsvReader from "../csvHandler/CsvReader";
 import GetIDS from "../csvHandler/getIDs";
-import ForgetPassword from "./forgetPassword";
-import Footer from "./footer";
-import ChangePassword from "./changePassword";
-import UploadFileC from "./trainer/VideoUpload/uploadFile";
-import VideoNavC from "./trainer/VideoUpload/uploadVideoNav";
-import MyVideosC from "./trainer/VideoUpload/myVideos";
-import TestsView from "./trainer/aymen/TestsView";
-import StatsView from "./trainer/aymen/StatsView";
-import EvaluationsView from "./trainer/aymen/EvaluationsView";
-import UploadConsent from "./UploadConsent";
-import AcceptTerms from "./acceptTerms";
-import ApproveTests from "./approveTests";
-import EmailConfirmed from "./emailConfirmed";
-import DisplayStudyAccept from "../csvHandler/displayStudyAccept";
+import RolesRoute from "../rolesRoute";
+import { useLocation } from "react-router";
+import {useEffect} from 'react';
 
 function Main() {
-    const [nvLogin, setnvLogin] = useState(false);
+    const [nvLogin, setnvLogin] = useState(true);
     const onLoginF = () => {
         setnvLogin(true);
     }
     const onNavBar = () => {
         setnvLogin(false);
     }
+   const location = useLocation();
+
+    useEffect(() => {
+        console.log("location:", location);
+        if(location && location.search && location.search.indexOf("code") !== -1) {
+            localStorage.setItem('url', location.search);
+        }
+    }, [location]);
+
     return (
             <div className="App">
-
                 <NavBar loggedin={nvLogin} navBarUpdated={onNavBar}/>
                 <div className="auth-wrapper">
-                        <Routes>
+                       <Routes>
                             <Route exact path='/' element={<div className="auth-inner"><Login/></div>} />
                             <Route exact path='/reg' element={<div className="auth-inner"> <Login/></div>} />
                             <Route path='/reg/sign-in' element={<div className="auth-inner"> <Login onLogin={onLoginF}/></div>} />
@@ -59,16 +57,10 @@ function Main() {
                             <Route path='/reg/sign-out' element={<div className="auth-inner"><SignOut/></div>} />
                             <Route path='/reg/regSuc' element={<div className="auth-inner"><AfterReg/></div>} />
                             <Route path='/reg/profile' element={<div className="auth-inner"><MyProfile/></div>} />
-                            <Route path='/reg/forgetPassword' element={<div className="csv-inner"><ForgetPassword/></div>} />
-                            <Route path='/reg/changeMyPassword' element={<div className="csv-inner"><ChangePassword/></div>} />
-                            <Route path='/reg/uploadConsent' element={<div className="auth-inner"><UploadConsent/></div>} />
-                            <Route path='/reg/accept' element={<div className="auth-inner"><AcceptTerms/></div>} />
-                            <Route path='/reg/emailConfirmed' element={<div className="auth-inner"><EmailConfirmed/></div>} />
-
-
-                            <Route path='/reg/approveTests' element={<div className="auth-inner"><ApproveTests/></div>} />
-
-                            <Route path='/lime/control' element={<div className="auth-inner"><LimeControl/></div>} />
+                            <Route path='/lime/control' element={
+                                <RolesRoute roles={['sport_admin']}>
+                                    <div className="auth-inner"> <LimeControl/></div>
+                                </RolesRoute> } />
                             <Route path='/reg/updateProfile' element={<div className="auth-inner"><UpdateProfile/></div>} />
                             <Route path='/trainer/sheet' element={<div className="auth-inner"><TrainerSheet/></div>} />
                             <Route path='/trainer/createTest' element={<div className="auth-inner"><CreateTest/></div>} />
@@ -76,28 +68,10 @@ function Main() {
                             <Route path='/trainer/adminMain' element={<div className="auth-inner"><CreateTest/></div>} />
                             <Route path='/trainer/addAthletes' element={<div className="auth-inner"><AddAthlete/></div>} />
                             <Route path='/trainer/editCoach' element={<div className="auth-inner"><EditCoach/></div>} />
-                            <Route path='/trainer/VideoUpload/uploadFile' element={<div className="auth-inner"><UploadFileC/></div>} />
-                            <Route path='/trainer/VideoUpload/videonav' element={<div className="auth-inner"><VideoNavC/></div>} />
-                            <Route path='/trainer/VideoUpload/myvideos' element={<div className="csv-inner"><MyVideosC/></div>} />
-
-
                             <Route path='/csv/reader' element={<div className="csv-inner"><CsvReader/></div>} />
                             <Route path='/csv/athleteInfo' element={<div className="csv-inner"><GetIDS/></div>} />
-                            <Route path='/csv/history' element={<div className="csv-inner data-view"><TestsView/></div>} />
-                            <Route path='/csv/stats' element={<div className="csv-inner data-view"><StatsView/></div>} />
-                            <Route path='/csv/displayAccept' element={<div className="wide-inner data-view"><DisplayStudyAccept/></div>} />
-
-                            <Route path='/trainer/myhistory' element={<div className="csv-inner data-view"><EvaluationsView/></div>} />
-
-
-
-
-
-
                         </Routes>
                 </div>
-                <div><p></p></div>
-                <Footer />
 
             </div>
     );
